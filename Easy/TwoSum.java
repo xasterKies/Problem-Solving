1f1066037a57f09ff9b7ddeb4a0df758d736283a
xaster-Kies <samueledward40@gmai.com>
2021-07-15 13:28:47 +0100

    two sum

---
+++ Easy/TwoSum.java
@@ -0,0 +1,14 @@
+package Easy;
+
+/* Author: Tiokeng Samuel
+ * Number Problem on Leetcode: 1. TwoSum
+ * Date: July 12th, 2021
+ */
+
+public class TwoSum {
+    public static void main(String[] args) {
+        
+    }
+
+    
+}
