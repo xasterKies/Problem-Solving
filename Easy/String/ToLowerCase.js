9dcb55dee6a9cd6ecced5a387c431d1accefcab0
xaster-Kies <samueledward40@gmail.com>
2022-01-04 12:26:58 +0100

    toLowerCase

---
+++ Easy/String/ToLowerCase.js
@@ -0,0 +1,30 @@
+// Author: Samuel Tiokeng
+// Title: Convert to lowercase without the .toLowerCase
+// Problem Description: https://leetcode.com/problems/to-lower-case/
+// Date: 1st January 2022
+
+var toLowerCase = function (str) {
+  return str
+    .split("")
+    .map(s => isUpperCase(s)
+      ? toLower(s)
+      : s)
+    .join("");
+};
+
+const isUpperCase = char => {
+  let ascii = char.charCodeAt(0);
+
+  if (ascii >= 65 && ascii <= 90) {
+    return true;
+  }
+  return false
+}
+
+const toLower = char => {
+  let ascii = char.charCodeAt(0)
+
+  ascii = String.fromCharCode(ascii + 32);
+
+  return ascii
+}
