f44099648d06b7e0e458f732419fccac01f9adc9
xaster-Kies <samueledward40@gmail.com>
2022-01-05 18:10:42 +0100

    length of last word

--- Easy/String/LengthOfLastWord.js
+++ Easy/String/LengthOfLastWord.js
@@ -3,3 +3,12 @@
  * Description: https://leetcode.com/problems/length-of-last-word/
  * Date: 5th January 2022
  */
+
+
+function lengthOfLastWord(s) {
+    let word = s.trim().split(" ");
+
+    return word.length > 0 ? word[word.length - 1].length : 0
+}
+
+console.log(lengthOfLastWord("Hello Word"))
